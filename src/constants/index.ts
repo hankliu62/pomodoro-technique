4de5446511433faa72ce262c84ff79f4e94844de
hankliu <397694072@qq.com>
2024-05-13 01:02:36 +0800

    feat(pomodoro-technique): add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,21 @@
+export const PageTitle = 'H.L Toolkits - 番茄工作法';
+
+export const PageSubTitle = `在忙碌的现代生活中，番茄工作法为我们提供了一把独特的工作利器。在这个看似快节奏的世界里，番茄工作法为我们创造了一片宁静的工作天地。通过将工作划分为短暂的番茄时间段，我们可以更加专注地投入到工作中，充分发挥自己的潜能。`;
+
+export const PageDescription = `番茄工作法网站旨在向您展示如何有效地运用番茄工作法，提高工作效率，增强工作质量。让我们一起在这个小番茄的世界里，创造更加美好的工作体验吧！`;
+
+export const PageKeywords = [
+  'pomodoro technique',
+  '番茄工作法',
+  'toolkit',
+  'toolkits',
+  '前端开发',
+  '前端开发工具',
+  '前端开发工具集合',
+  '在线工具',
+  'toolbox',
+  'frontend',
+  '卡鲁秋',
+  'Hank',
+  'HankLiu',
+];
